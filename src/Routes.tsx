3c965b49db242322314daa25f015a14ce990d483
wdwf <w_dwf@hotmail.com>
2024-02-11 18:02:43 -0300

    :construction: chore: alteracao na nomenclatura das rotas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,13 +6,15 @@ import {
 
 import Home from './pages/Home';
 import Cap2 from "./pages/Cap2";
+import Cap3 from "./pages/Cap3";
 
 export function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/capitulo2" element={<Cap2/>} />
+        <Route path="/capitulo-2" element={<Cap2/>} />
+        <Route path="/capitulo-3" element={<Cap3/>} />
       </Routes>
     </Router>
   )
